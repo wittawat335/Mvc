eae6df69d1f76fe8d97169b1c0120c905299aa98
คอม <@>
2016-06-02 17:40:54 +0700

    admin Product,Supplier 80%

---
+++ ShoppingComplex/ShoppingComplex/Areas/Admin/Controllers/SuppliersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingComplex.Models;
+
+namespace ShoppingComplex.Areas.Admin.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private ShoppingDbContext db = new ShoppingDbContext();
+
+        // GET: Admin/Suppliers
+        public ActionResult Index()
+        {
+            ViewBag.Suppliers = db.Suppliers.ToList();
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        // POST: Admin/Suppliers/Upload
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Content/img/suppliers/"),
+                                               Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return View();
+        }
+
+        // GET: Admin/Suppliers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Suppliers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Logo,Email,Phone")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(supplier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Admin/Suppliers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Admin/Suppliers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Logo,Email,Phone")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supplier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
+
+        // GET: Admin/Suppliers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supplier supplier = db.Suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
+
+        // POST: Admin/Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Supplier supplier = db.Suppliers.Find(id);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
