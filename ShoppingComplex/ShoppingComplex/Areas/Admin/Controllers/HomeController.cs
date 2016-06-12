108f167540029e6e3edd14706cd2a583e1a0f108
คอม <@>
2016-06-12 20:48:13 +0700

    Admin Reenue

--- ShoppingComplex/ShoppingComplex/Areas/Admin/Controllers/HomeController.cs
+++ ShoppingComplex/ShoppingComplex/Areas/Admin/Controllers/HomeController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShoppingComplex.Models;
 
 namespace ShoppingComplex.Areas.Admin.Controllers
 {
     
     public class HomeController : Controller
     {
-        //
+        ShoppingDbContext db = new ShoppingDbContext();
         // GET: /Admin/Home/
         public ActionResult Index()
         {
+            ViewBag.Order = db.Orders.Count();
+            ViewBag.Customers = db.Customers.Count();
+            ViewBag.Products = db.Products.Count();
+            ViewBag.Categories = db.Categories.Count();
             return View();
         }
 	}
